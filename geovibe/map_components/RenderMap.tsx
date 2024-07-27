37fb8f41408d06b8b15cafbd4eb0d54f576dd388
JaylenHoLuc <jay13857@esri.com>
2024-07-27 14:20:42 -0700

    pins

--- geovibe/map_components/RenderMap.tsx
+++ geovibe/map_components/RenderMap.tsx
@@ -6,45 +6,117 @@ import Extent from '@arcgis/core/geometry/Extent'
 import { watch } from '@arcgis/core/core/reactiveUtils'
 import Map from '@arcgis/core/Map';
 import Expand from '@arcgis/core/widgets/Expand'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styles from './styles.module.css'
-const RenderMap = () => {
+import Graphic from "@arcgis/core/Graphic";
+import Point from "@arcgis/core/geometry/Point";
+import * as webMercatorUtils from "@arcgis/core/geometry/support/webMercatorUtils.js";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer.js";
+
+
+const RenderMap = ({start_x, start_y} :{start_x : number, start_y : number}) => {
+    
+    const [currentPointer, setPointer] = useState<Graphic | null>(null);
+    let graphicsLayer : GraphicsLayer | null = null;
+    
+    
+    const addPoint = (view :  MapView | null , x : number, y : number) => {
+       
+            const point = new Point({
+                longitude: x,
+                latitude: y
+            });
+            
+            const markerSymbol = {
+                type: "simple-marker", // Use a simple marker symbol
+                color: [226, 119, 40], // Set marker color
+                outline: {
+                color: [255, 255, 255], // Set outline color
+                width: 2
+                }
+            };
+            
+            const pointGraphic = new Graphic({
+                geometry: point,
+                symbol: markerSymbol
+            });
+            if (currentPointer && graphicsLayer){
+                // const graphicToRemove = graphicsLayer.graphics.find(
+                //     (graphic) => graphic.attributes.id === "point-id"
+                //   );
+                const graphics = graphicsLayer.graphics;
+                if (graphics.length > 0) {
+                  graphicsLayer.remove(graphics.getItemAt(graphics.length - 1));
+                }
+            }
+
+            setPointer(pointGraphic);
+            graphicsLayer!.add(pointGraphic);
+
+            //movePoint(view, x , y);
+            // view!.graphics.add(currentPointer);
+          
+        
+       
+    }
+    const movePoint = (view :  MapView | null, x : number , y : number) => {
+        if (currentPointer && view) {
+            currentPointer.setAttribute("longitude", y);
+            currentPointer.setAttribute("latitude", x)
+            const temp = currentPointer;
+            setPointer(temp);
+            console.log("removed")
+        }
+    }
 
     const mapRef = useRef(null);
 
     useEffect(() => {
-      let view : MapView | null;
-      (async () => {
+        let view : MapView | null;
+        
 
-        const map = new Map({
-          basemap: 'topo-vector'
-        });
-  
-        view = new MapView({
-          container: mapRef.current!,
-          map: map,
-          center: [-118.80500, 34.02700],
-          zoom: 3,
-          constraints: {
-            minZoom: 3
-          }
-          
-        });
-        const postWidget =  document.getElementById("post-wrap")!;
-        const expand = new Expand({
-            content: postWidget,
-            expanded: false
-        })
-        view.ui.add(expand, "top-right")
-    
-      })();
-  
+        const initMap = async () => {
+            const map = new Map({
+                basemap: 'topo-vector'
+            });
+
+            view = new MapView({
+                container: mapRef.current!,
+                map: map,
+                center: [start_x, start_y],
+                zoom: 3,
+                constraints: {
+                minZoom: 3
+                }
+                
+            });
+            const postWidget =  document.getElementById("post-wrap")!;
+            const expand = new Expand({
+                content: postWidget,
+                expanded: false
+            })
+
+            graphicsLayer = new GraphicsLayer();
+            map.add(graphicsLayer);
+            view.ui.add(expand, "top-right")
+            view.on("click", function(event) {
+                // Get the coordinates of the clicked point
+                const mapPoint = event.mapPoint;
+                const x = mapPoint.longitude;
+                const y = mapPoint.latitude;
+                console.log("currentpointer: ", currentPointer)
+                addPoint(view, x, y);
+                console.log("Clicked coordinates:", x, y);
+                });
+            }   
+        initMap();     
       return () => {
         if (view) {
-          view.destroy();
-          view = null;
+            view.destroy();
+            view = null;
         }
       };
+      
     }, []);
   
     return (
--- geovibe/pages/post/index.tsx
+++ geovibe/pages/post/index.tsx
@@ -5,7 +5,7 @@ import styles from "@/styles/Home.module.css";
 import RenderMap from "@/map_components/RenderMap";
 import dynamic from "next/dynamic";
 import Navbar from "@/navbar_components/navbar";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,10 +24,16 @@ export default function Post() {
       pad2( date.getMinutes() ) + ":" +
       pad2( date.getSeconds() ));
     }
-  
+    
+    const [currentCategory, setCurrentCategory] = useState("Select category");
     const handleSubmit = () => {
 
     }
+    const saveCategory = (category : string) => {
+        setCurrentCategory(category);
+
+    }
+    
     const mapRef = useRef(null);
 
     useEffect(() => {
@@ -90,7 +96,24 @@ export default function Post() {
           type="file"
           className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
       </label>
+
       </div>
+
+      <div id= "dropdowncat" className="dropdown dropdown-top">
+        <div tabIndex={0} role="button" className="btn m-1">{currentCategory}</div>
+        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+          <li onClick={() => saveCategory("Nature")}><a>Nature</a></li>
+          <li onClick={() => saveCategory("Travel")}><a>Travel</a></li>
+          <li onClick={() => saveCategory("Food")}><a>Food</a></li>
+          <li onClick={() => saveCategory("The Arts")}><a>The Arts</a></li>
+          <li onClick={() => saveCategory("Music")}><a>Music</a></li>
+          <li onClick={() => saveCategory("Event")}><a>Event</a></li>
+          <li onClick={() => saveCategory("Architecture")}><a>Architecture</a></li>
+          <li onClick={() => saveCategory("Sport")}><a>Sport</a></li>
+        </ul>
+      </div>
+
+
       <div>
         <label className="form-control w-full max-w-xs">
             <div className="label">
