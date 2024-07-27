80c1c46d362ef04efdbf36cd198b4f9e70d727da
JaylenHoLuc <jay13857@esri.com>
2024-07-27 10:13:41 -0700

    post skeleton

--- geovibe/pages/_app.tsx
+++ geovibe/pages/_app.tsx
@@ -1,8 +1,8 @@
-import Layout from '../components/layout'
+//import Layout from '../components/layout'
 import '@/styles/globals.css'
 import Head from "next/head";
 import Navbar from '@/navbar_components/navbar';
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps } : any) {
   return (
     <>
       <Head>
