c57a5a985a4e31e8f39a021ff7245f67c4b45f70
Antariksha Ray <ant13843@esri.com>
2024-07-27 15:55:32 -0700

    Move supabaseclient to lib from utils

--- geovibe/lib/supabaseclient.js
+++ geovibe/lib/supabaseclient.js
@@ -2,5 +2,8 @@ import { createClient } from '@supabase/supabase-js';
  
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
- 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default function createSupabaseClient() {
+    const supabase = createClient(supabaseUrl, supabaseKey);
+    return supabase
+}
