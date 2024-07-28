24ebfae331fca193590afecce73475de78db217c
Antariksha Ray <ant13843@esri.com>
2024-07-28 05:43:11 -0700

    Feed page with map and pins and popups

--- geovibe/lib/SupabaseHelper.tsx
+++ geovibe/lib/SupabaseHelper.tsx
@@ -12,4 +12,24 @@ const  getAllPosts = async () => {
 
 
 }
-export default getAllPosts;
+
+const  getAllPostsUnlimited = async () => {
+    const supabase = await createSupabaseClient()
+    const allPosts = await supabase.from('Posts')
+        .select()
+        .like("pic_uri", '%jpg%')
+        .order('created_at', {ascending : false});
+    return allPosts['data'];
+}
+
+const getRealNameFromUserName  = async (username: string) => {
+    const supabase = await createSupabaseClient()
+    const data = await supabase.from('Users').select().eq("username", username).limit(1)
+    return data.data[0].name
+}
+
+const getPublicPicUrl = async (filename: string) => {
+    const {data} = createSupabaseClient().storage.from('user-images').getPublicUrl(filename);
+    return data["publicUrl"]
+}
+export {getAllPosts, getAllPostsUnlimited, getRealNameFromUserName, getPublicPicUrl};
--- geovibe/pages/feed/index.tsx
+++ geovibe/pages/feed/index.tsx
@@ -2,16 +2,56 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import RenderMap from "@/map_components/RenderMap";
+import RenderMapFeed from "@/map_components/RenderMapFeed";
 import dynamic from "next/dynamic";
 import Navbar from "@/navbar_components/navbar";
+import { useEffect, useState } from "react";
+import { getAllPosts, getAllPostsUnlimited } from "@/lib/SupabaseHelper";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Feed() {
+    const [selectedP, setPosts] = useState<any[] | null>(null);
+    const [xs, setXs] = useState([])
+    const [ys, setYs] = useState([]);
+    const [all_posts, setAllPosts] = useState([])
+
+
+    const EsriMap = dynamic(() => import("@/map_components/RenderMapFeed"), { ssr: false });
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+        const allPosts = await getAllPostsUnlimited();
+        const ys = allPosts?.map(post => post.latitude);
+        const xs = allPosts?.map(post => post.longitude);
+        setXs(xs);
+        setYs(ys);
+        setAllPosts(allPosts);
+        // console.log(xs)
+        }
+        fetchPosts()
+    },[]);
+        
     return (
         <>
-            <h1>Feed Page</h1>
+        <div>
+            {/* <div className="col-span-1 place-items-center card bg-base-100">
+                <div className="post-feed">
+                    {xs && <></>}
+                    {!xs && <div className="post">
+                    <h2 className="post-title">Select a pin to see the post!</h2>
+                    </div>}
+                </div>
+            </div> */}
+            <div className="col-span-4">
+                <div className="card bg-base-100">
+                <EsriMap start_x={-118.80500} start_y={34.02700} point_ref={null} x_coords ={xs} y_coords={ys} allPosts={all_posts}/>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h2>Select a pin to see the post!</h2>
+        </div>
         </>
     );
 }
