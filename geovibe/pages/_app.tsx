413b48363a631f062cd008757f650a55aca8b504
Sartaj Dua <sar13940@c02cj0jwlvdm.esri.com>
2024-07-28 08:33:35 -0700

    added react-toastify

--- geovibe/pages/_app.tsx
+++ geovibe/pages/_app.tsx
@@ -2,10 +2,14 @@
 import '@/styles/globals.css'
 import Head from "next/head";
 import Navbar from '@/navbar_components/navbar';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function MyApp({ Component, pageProps } : any) {
   return (
     <>
       <Navbar></Navbar>
+      <ToastContainer />
       <Component {...pageProps} />
     </>
   )
